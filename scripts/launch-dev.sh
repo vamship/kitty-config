2f4dd66aeb6e8349ebd4260c6892279f929f8851
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2025-11-27 17:14:17 -0500

    Add basic configuration files

---
+++ scripts/launch-dev.sh
@@ -0,0 +1,6 @@
+kitty @ launch --type=tab --cwd ~/projects --tab-title "Code"
+kitty @ goto-layout --match id:-1 splits
+kitty @ launch --type=window --cwd ~/projects --location=vsplit --bias=45
+kitty @ goto-layout --match id:-1 splits
+kitty @ launch --type=window --cwd ~/projects --location=hsplit --bias=30
+kitty @ goto-layout --match id:-1 splits
